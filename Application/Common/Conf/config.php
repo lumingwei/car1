5800fb66533b002f4d03a33677b029828d3b722b
明炜 卢 <307958040@qq.com>
2020-05-27 00:35:32 +0800

    1

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,5 +1,25 @@
 <?php
 return array(
+    //'配置项'=>'配置值'
+    'DB_TYPE'=>'mysql',      //数据库类型
+    'DB_HOST'=>'111.229.216.178',  //服务器地址
+    //'DB_NAME'=>'war',       //数据库名
+    'DB_NAME'=>'tools',       //数据库名
+    'DB_USER'=>'root',       //数据库账号
+    'DB_PWD'=>'@Lumingwei666',      //数据库密码
+    'DB_PORT'=>3306,         //数据库端口
+    //'DB_PREFIX'=>'war_',      //数据库表前缀
+    'DB_PREFIX'=>'tool_',      //数据库表前缀
+    'DB_CHARSET'=>'utf8',    //数据库字符编码
+
+    'REDIS_HOST'=>'111.229.216.178',
+    'REDIS_PORT'=>6379,
+    'DATA_CACHE_TIMEOUT'=>10,
+    'REDIS_AUTH_PASSWORD'=>'redislmw123',
+
+    'URL_MODEL'=>0
+);
+/*return array(
 	//'配置项'=>'配置值'
     'DB_TYPE'=>'mysql',      //数据库类型
     'DB_HOST'=>'127.0.0.1',  //服务器地址
@@ -10,6 +30,6 @@ return array(
     'DB_PREFIX'=>'ta_',      //数据库表前缀
     'DB_CHARSET'=>'utf8',    //数据库字符编码
     'URL_MODEL'=>0
-);
+);*/
 
 
